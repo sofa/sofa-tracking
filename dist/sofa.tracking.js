034dc80272d33dd6847246e765032486c55eb1d9
Nick Janssen <info@nickjanssen.com>
2014-03-12 15:26:04 +0100

    refactor(googleAnalyticsTracker): move conversion tracking to trackTransaction

--- dist/sofa.tracking.js
+++ dist/sofa.tracking.js
@@ -136,14 +136,6 @@ sofa.define('sofa.tracking.GoogleAnalyticsTracker', function (options) {
             if (eventData.value) {
                 dataToBePushed.push(eventData.value);
             }
-
-            if (eventData.action === 'google_conversion' && options.conversionId) {
-                var url = 'http://www.googleadservices.com/pagead/conversion/' +
-                    options.conversionId + '/?value=' + eventData.value + '&label=' +
-                    options.conversionLabel + '&guid=ON&script=0';
-                var image = new Image(1, 1);
-                image.src = url;
-            }
         }
 
         _gaq.push(dataToBePushed);
@@ -159,6 +151,15 @@ sofa.define('sofa.tracking.GoogleAnalyticsTracker', function (options) {
      * @param {object} transactionData Transaction data object.
      */
     self.trackTransaction = function (transactionData) {
+
+        if (options.conversionId) {
+            var url = 'http://www.googleadservices.com/pagead/conversion/' +
+                options.conversionId + '/?value=' + transactionData.totals.subtotal + '&label=' +
+                options.conversionLabel + '&guid=ON&script=0';
+            var image = new Image(1, 1);
+            image.src = url;
+        }
+
         _gaq.push(['_gat._anonymizeIp']);
         _gaq.push(['_addTrans',
             transactionData.token,               // transaction ID - required
